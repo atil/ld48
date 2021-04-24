e1292c0b2e20a2946264db763d1ee98673b4a7aa
eusekerci <enesugursekerci@gmail.com>
2021-04-24 23:44:19 +0200

    move slider with tween

--- Assets/Scripts/Game/GameUi.cs
+++ Assets/Scripts/Game/GameUi.cs
@@ -1,4 +1,5 @@
-﻿using JamKit;
+﻿using System.Collections;
+using JamKit;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -12,6 +13,9 @@ namespace Game
         [SerializeField] private TextMeshProUGUI _gemText;
         [SerializeField] private Slider _slider;
         
+        public AnimationCurve SliderMoveCurve;
+        private const float _sliderMoveDuration = 0.5f;
+
         void Start()
         {
             Flash(_openFlashInfo);
@@ -20,14 +24,24 @@ namespace Game
         public void SetOxygen(int oxy)
         {
             _oxygenText.text = $"{oxy.ToString()}";
-            _slider.value = oxy;
+            
+            int initValue = (int)_slider.value;
+            
+            Curve.Tween(SliderMoveCurve,
+                _sliderMoveDuration,
+                t =>
+                {
+                    _slider.value = Mathf.Lerp(initValue, oxy, t);
+                },
+                () =>
+                {
+                    _slider.value = oxy;
+                });
         }
         
         public void SetGem(int gem)
         {
             _gemText.text = $"{gem.ToString()}";
         }
-        
-        
     }
 }
