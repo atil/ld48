22b5fd53679c11ef5f686ecbabeb2f01c668c639
eusekerci <enesugursekerci@gmail.com>
2021-04-24 18:59:40 +0200

    initial random tile types

--- Assets/Scripts/Game/TileGenerator.cs
+++ Assets/Scripts/Game/TileGenerator.cs
@@ -1,11 +1,40 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Game;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class TileGenerator : MonoBehaviour
 {
     public Transform TilePrefab;
     public Transform TileRoot;
+
+    private Dictionary<TileType, Sprite> _sprites;
+    private Sprite[] _waterSprites;
+
+    private void Awake()
+    {
+        _waterSprites = Resources.LoadAll<Sprite>("Textures/Water");
+        _sprites = new Dictionary<TileType, Sprite>
+        {
+            {
+                TileType.Water,
+                _waterSprites[23]
+            },
+            {
+                TileType.Gem,
+                Resources.Load<Sprite>("Textures/Gem")
+            },
+            {
+                TileType.Oxygen,
+                Resources.Load<Sprite>("Textures/Tank")
+            },
+            {
+                TileType.Shark,
+                Resources.Load<Sprite>("Textures/Shark")
+            }
+        };
+    }
 
     public List<Tile> GenerateTiles(int rowAmount, int columnAmount)
     {
@@ -16,6 +45,13 @@ public class TileGenerator : MonoBehaviour
             {
                 var newTile = Instantiate(TilePrefab, transform.position, Quaternion.identity);
                 newTile.parent = TileRoot;
+                
+                var tileData = newTile.GetComponent<Tile>();
+                tileData.Type = (TileType)Random.Range(0, 4);
+                tileData.Value = Random.Range(1, 4);
+                
+                newTile.Find("Visual").GetComponent<SpriteRenderer>().sprite = _sprites[tileData.Type];
+                
                 result.Add(newTile.GetComponent<Tile>());
             }
         }
