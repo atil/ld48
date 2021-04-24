e3b984d40f390be6ef7126da76b37e199046c548
atil <hyperion91@gmail.com>
2021-04-24 16:46:01 +0200

    Clicking on stuff

--- Assets/Scripts/Game/GameMain.cs
+++ Assets/Scripts/Game/GameMain.cs
@@ -1,8 +1,58 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using JamKit;
+using UnityEngine;
 
 namespace Game
 {
     public class GameMain : MonoBehaviour
     {
+        public List<Tile> TempTiles;
+        public Player Player;
+        public AnimationCurve MoveCurve;
+        
+        private List<Tile[]> _tiles = new List<Tile[]>();
+        private bool _isMoving;
+        
+        private void Start()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                _tiles.Add(new Tile[4]);
+                for (int j = 0; j < 4; j++)
+                {
+                    _tiles[i][j] = TempTiles[i * 4 + j];
+                    _tiles[i][j].Index = (i, j);
+                }
+            }
+            
+        }
+
+        public void OnTileClicked(Tile tile)
+        {
+            if (_isMoving)
+            {
+                return;
+            }
+            
+            Vector3 srcPos = Player.transform.position;
+            Vector3 targetTilePos = tile.transform.position;
+            
+            _tiles[tile.Index.i][tile.Index.j] = null;
+            Destroy(tile.gameObject);
+
+            _isMoving = true;
+            Curve.Tween(MoveCurve,
+                0.5f,
+                t =>
+                {
+                    Player.transform.position = Vector3.Lerp(srcPos, targetTilePos, t);
+                },
+                () =>
+                {
+                    Player.transform.position = targetTilePos;
+                    _isMoving = false;
+                });
+        }
     }
 }
---
+++ Assets/Scripts/Game/Tile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game
+{
+    public class Tile : MonoBehaviour
+    {
+        public (int i, int j) Index { get; set; }
+        
+        private GameMain _game;
+
+        void Start()
+        {
+            _game = FindObjectOfType<GameMain>();
+        }
+        
+        private void OnMouseEnter()
+        {
+        }
+        
+        private void OnMouseExit()
+        {
+        }
+
+        private void OnMouseDown()
+        {
+            _game.OnTileClicked(this);
+        }
+    }
+}
